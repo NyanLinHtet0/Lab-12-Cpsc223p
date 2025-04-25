30461591b54d5f8d3b15bb28c3b197649a0d8f22
NyanLinHtet0 <harrishtet.nyanlinhtet@gmail.com>
2025-04-24 22:28:08 -0700

    Fixed naming variables so they become more clear

--- client.py
+++ client.py
@@ -1,25 +1,27 @@
 import json
 
-#TODOS: add error checking.
+#TODOS: add error checking
+#TODOS: check duplicate entries and reject them
+
 
 class Client():
     #class variable dictionary to store accounts in runtime
-    data = {}
+    account_data = {}
     def __init__(self, name, password):
         self.__name = name
         self.__password = password
-        Client.data[name]=password
+        Client.account_data[name]=password
         
     #class method to save all account names
     def save_all(filename="account_management.json"):
         with open(filename, "w") as f:
-            json.dump(Client.data, f, indent=4)
+            json.dump(Client.account_data, f, indent=4)
     
     #class method to read all accounts from json to runtime
     def read_all(filename="account_management.json"):
         with open(filename, "r") as f:
-            Client.data = json.load(f)
-        print(Client.data)  
+            Client.account_data = json.load(f)
+        print(Client.account_data)  
             
     
 Client("jack","pw123")        
